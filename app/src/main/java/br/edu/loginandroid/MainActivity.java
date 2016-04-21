72cd9343a6800e174a47db4ad0ab2be7a5662afc
Álisson Gomes Linhares <linhares.alisson@gmail.com>
2016-04-21 20:30:59 -0300

    Transição entre telas e Login com Firebase - código oficial dos slides 3

--- app/src/main/java/br/edu/loginandroid/MainActivity.java
+++ app/src/main/java/br/edu/loginandroid/MainActivity.java
@@ -1,5 +1,7 @@
 package br.edu.loginandroid;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,12 +21,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    // FIREBASE
-    public static final String FIREBASE_REPO = "blinding-heat-9190";
-    public static final String FIREBASE_URL = "https://" + FIREBASE_REPO + ".firebaseio.com";
-    private Firebase firebase;
-
-    // LOGIN
+    // TRANSIÇÃO ENTRE TELAS
     private EditText editTextLogin;
     private EditText editTextPassword;
     private Switch switchFirebase;
@@ -33,12 +30,22 @@ public class MainActivity extends AppCompatActivity {
     private String login;
     private String password;
 
+    // FIREBASE
+    public static final String FIREBASE_REPO = "blinding-heat-9190";
+    public static final String FIREBASE_URL = "https://" + FIREBASE_REPO + ".firebaseio.com";
+    private Firebase firebase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TRANSIÇÃO ENTRE TELAS
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginSenha.put("edu@email.com", "edu");
+        editTextLogin = (EditText) findViewById(R.id.editTextLogin);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        switchFirebase = (Switch) findViewById(R.id.switchFirebase);
 
         // FIREBASE
         if (savedInstanceState == null){
@@ -47,64 +54,27 @@ public class MainActivity extends AppCompatActivity {
         // Create a connection to your Firebase database
         firebase = new Firebase(FIREBASE_URL);
 
-        // LOGIN
-        loginSenha.put("edu@email.com", "edu");
-        editTextLogin = (EditText) findViewById(R.id.editTextLogin);
-        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
-        switchFirebase = (Switch) findViewById(R.id.switchFirebase);
-
         // LOGIN COM FIREBASE
+        AuthData authData = firebase.getAuth();
+        if (authData != null) {
+            Log.d("authData: ", "usuário autenticado");
+        } else {
+            Log.d("authData: ", "usuário NÃO autenticado");
+        }
         firebase.addAuthStateListener(new Firebase.AuthStateListener() {
             @Override
             public void onAuthStateChanged(AuthData authData) {
                 if (authData != null) {
                     Log.d("onAuthStateChanged: ", "usuário logado");
-                    // user is logged in
                 } else {
                     Log.d("onAuthStateChanged: ", "usuário não logado");
-                    // user is not logged in
                 }
             }
         });
-        AuthData authData = firebase.getAuth();
-        if (authData != null) {
-            // user authenticated
-            Log.d("authData: ", "usuário autenticado");
-        } else {
-            // no user authenticated
-            Log.d("authData: ", "usuário NÃO autenticado");
-        }
     }
 
     // TRANSIÇÃO ENTRE TELAS
-    private void transitaParaTelaFirebaseShared() {
-        setContentView(R.layout.status_firebase_shared);
-
-        textViewStatus = (TextView) findViewById(R.id.textViewStatus);
-
-        // FIREBASE
-        // Listen for realtime changes
-        firebase.child("status").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snap) {
-                textViewStatus.setText(snap.getValue().toString());
-            }
-            @Override public void onCancelled(FirebaseError error) { }
-        });
-
-    }
-
-    // FIREBASE
-    public void felizClick (View v){
-        firebase.child("status").setValue("Feliz");
-    }
-    public void tristeClick (View v){
-        firebase.child("status").setValue("Triste");
-    }
-
-    // LOGIN
     public void signin (View v){
-        // LOGIN
         login = editTextLogin.getText().toString();
         password = editTextPassword.getText().toString();
 
@@ -123,23 +93,47 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // TRANSIÇÃO ENTRE TELAS
+    private void transitaParaTelaFirebaseShared() {
+        setContentView(R.layout.status_firebase_shared);
+
+        textViewStatus = (TextView) findViewById(R.id.textViewStatus);
+
+        // FIREBASE
+        // Listen for realtime changes
+        firebase.child("status").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snap) {
+                textViewStatus.setText(snap.getValue().toString());
+            }
+            @Override public void onCancelled(FirebaseError error) { }
+        });
+    }
+
+    // FIREBASE
+    public void felizClick (View v){
+        firebase.child("status").setValue("Feliz");
+    }
+    public void tristeClick (View v){
+        firebase.child("status").setValue("Triste");
+    }
+
     // LOGIN COM FIREBASE
     private void loginComFirebase() {
-
         // CRIA USUÁRIO NO FIREBASE COM DADOS PRIVADOS A PARTIR DE EMAIL E SENHA
         firebase.createUser(login, password, new Firebase.ValueResultHandler<Map<String, Object>>() {
             @Override
             public void onSuccess(Map<String, Object> result) {
+                editTextLogin.setError("Usuário criado com sucesso! Por favor, tente novamente.");
                 Log.d("onSucess: ", "Successfully created user account with uid: " + result.get("uid"));
             }
             @Override
             public void onError(FirebaseError firebaseError) {
-                // there was an error
                 Log.d("onError: ", "Autenticação falhou, " + firebaseError.getMessage());
             }
         });
 
-        // REALIZA LOGIN NO FIREBASE
+        // REALIZA LOGIN COM EMAIL E SENHA NO FIREBASE
         // Create a handler to handle the result of the authentication
         Firebase.AuthResultHandler authResultHandler = new Firebase.AuthResultHandler() {
             @Override
@@ -147,22 +141,16 @@ public class MainActivity extends AppCompatActivity {
                 // Authenticated successfully with payload authData
                 Map<String, String> map = new HashMap<String, String>();
                 map.put("provider", authData.getProvider());
-                if(authData.getProviderData().containsKey("displayName")) {
-                    map.put("displayName", authData.getProviderData().get("displayName").toString());
-                }
                 firebase.child("users").child(authData.getUid()).setValue(map);
+                // TRANSIÇÃO ENTRE TELAS
+                transitaParaTelaFirebaseShared();
             }
             @Override
             public void onAuthenticationError(FirebaseError firebaseError) {
-                // Authenticated failed with error firebaseError
                 Log.d("authData: ", "Autenticação falhou, " + firebaseError.getMessage());
+                editTextPassword.setError(firebaseError.getMessage());
             }
         };
         firebase.authWithPassword(login, password, authResultHandler);
-
-        // TRANSIÇÃO ENTRE TELAS
-        transitaParaTelaFirebaseShared();
-
      }
-
 }
